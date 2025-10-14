050bed900e7b1c96d56f188416ea5ce20ab64b61
Polina <polina.solodkova@gmail.com>
2025-10-14 15:43:05 +0300

    fix: edit file with prettier

--- src/apollo.tsx
+++ src/apollo.tsx
@@ -1,4 +1,4 @@
-import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
+import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
 
 const API_URL = import.meta.env.VITE_API_URL;
 
