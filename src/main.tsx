790f3cb42b8bac066f472ef511f5a3de4e4ee45d
Polina <polina.solodkova@gmail.com>
2025-10-14 14:34:21 +0300

    feat: setup apollo client

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+import * as ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
+import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
+import { ApolloProvider } from '@apollo/client/react';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+const client = new ApolloClient({
+  link: new HttpLink({ uri: 'https://cv-project-js.inno.ws/api/graphql' }),
+  cache: new InMemoryCache(),
+});
+
+const root = ReactDOM.createRoot(document.getElementById('root')!);
+
+root.render(
+  <ApolloProvider client={client}>
     <App />
-  </StrictMode>,
+  </ApolloProvider>,
 );
