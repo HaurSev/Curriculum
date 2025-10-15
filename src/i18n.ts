f907a9f48e1801a033b97c6be133e21e6c556001
Polina <polina.solodkova@gmail.com>
2025-10-15 11:19:31 +0300

    fix: rewrite i18n file with resourcesToBackend

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,18 +1,21 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-
-import Backend from 'i18next-http-backend';
+import resourcesToBackend from 'i18next-resources-to-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
 i18n
-  .use(Backend)
+  .use(
+    resourcesToBackend((language: string, namespace: string) =>
+      fetch(`/locales/${language}/${namespace}.json`).then((res) => res.json()),
+    ),
+  )
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
     fallbackLng: 'en',
     debug: true,
     lng: 'en',
-    ns: ['authorisation'],
+    ns: [],
     backend: {
       loadPath: '../public/locales/{{lng}}/{{ns}}.json',
     },
