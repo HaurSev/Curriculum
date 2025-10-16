26a4d036c2cf383ad4c0cbf653988be1fa3a4d73
Polina <polina.solodkova@gmail.com>
2025-10-16 17:40:09 +0300

    fix: clear App files

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,14 @@
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
-import { useTranslation } from 'react-i18next';
 import { client } from './apollo/apollo.tsx';
 import { ApolloProvider } from '@apollo/client/react';
-import useTestStore from './store/testStore.ts';
 import './i18n.ts';
+import router from './router/index.tsx';
+import { RouterProvider } from 'react-router-dom';
 
 function App() {
-  const { count, increment, reset } = useTestStore();
-
-  const { t, i18n } = useTranslation('common');
-
-  const handleChangeLanguage = () => {
-    const newLanguage = i18n.language === 'ru' ? 'en' : 'ru';
-    i18n.changeLanguage(newLanguage);
-  };
-
   return (
     <ApolloProvider client={client}>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-
-      <h1>Vite + React</h1>
-
-      <div className="card">
-        <button onClick={increment}>count is {count}</button>
-        <button onClick={reset}>reset</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <button onClick={handleChangeLanguage}>translate</button>
-      <p>{t('one')}</p>
+      <RouterProvider router={router} />
     </ApolloProvider>
   );
 }
