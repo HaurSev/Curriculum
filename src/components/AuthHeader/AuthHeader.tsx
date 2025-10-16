29aa370d3aace88a6a71bc481ab2f74c263cd4ea
Polina <polina.solodkova@gmail.com>
2025-10-16 17:52:32 +0300

    fix: add interactivity for header

--- src/components/AuthHeader/AuthHeader.tsx
+++ src/components/AuthHeader/AuthHeader.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import './AuthHeader.css';
+import { useNavigate } from 'react-router-dom';
+import { AppRoutes } from '../../router/router';
 
 interface AuthHeaderProps {
   active: 'login' | 'signup';
@@ -8,6 +10,8 @@ interface AuthHeaderProps {
 
 const AuthHeader: React.FC<AuthHeaderProps> = ({ active }) => {
   const { t } = useTranslation('authorisation');
+
+  const navigate = useNavigate();
 
   const logClassList = [];
   const signClassList = [];
@@ -20,11 +24,13 @@ const AuthHeader: React.FC<AuthHeaderProps> = ({ active }) => {
   return (
     <div className="authHeader">
       <button
+        onClick={() => navigate(AppRoutes.LOGIN)}
         className={logClassList.length === 0 ? '' : logClassList.join(' ')}
       >
         {t('login')}
       </button>
       <button
+        onClick={() => navigate(AppRoutes.REGISTRATION)}
         className={signClassList.length === 0 ? '' : signClassList.join(' ')}
       >
         {t('signup')}
