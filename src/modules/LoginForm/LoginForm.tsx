1f34e6590a5c55e21b85f825d53022279a30aedd
Polina <polina.solodkova@gmail.com>
2025-10-17 12:56:32 +0300

    feat: error display for login form

--- src/modules/LoginForm/LoginForm.tsx
+++ src/modules/LoginForm/LoginForm.tsx
@@ -3,19 +3,34 @@ import './LoginForm.css';
 import { useForm } from 'react-hook-form';
 import { useLogin } from '../../graphql/queries/login';
 import { useNavigate } from 'react-router-dom';
+import useErrorStore from '../../store/errorStore';
+import type { GraphQLError } from 'graphql';
+import type { ApolloError } from 'apollo-server-errors';
 
 type LoginFormData = {
   email: string;
   password: string;
 };
 
+interface ErrorExtensions {
+  response?: {
+    statusCode?: number;
+    message?: string | string[];
+  };
+}
+
 const LoginForm = () => {
   const { register, handleSubmit } = useForm<LoginFormData>();
   const { t } = useTranslation(['authorisation', 'common']);
   const navigate = useNavigate();
-  const [login, { loading }] = useLogin();
+  const [login, { loading, error }] = useLogin();
+  const { message, setError } = useErrorStore();
 
   const onSubmit = async (data: LoginFormData) => {
+    setError('');
+    if (!data.email || !data.password) {
+      setError('email or password no valide');
+    }
     try {
       const response = await login({
         variables: {
@@ -35,13 +50,33 @@ const LoginForm = () => {
   };
 
   return (
-    <form className="loginForm" onSubmit={handleSubmit(onSubmit)}>
+    <form
+      className="authForm"
+      autoComplete="off"
+      onSubmit={handleSubmit(onSubmit)}
+    >
       <input {...register('email')} type="text" placeholder={t('email')} />
       <input
         {...register('password')}
         type="password"
         placeholder={t('password')}
       />
+      <div className="error">
+        {message ? (
+          <p>{message}</p>
+        ) : error ? (
+          (error as ApolloError).errors.map((err: GraphQLError, i: number) => {
+            const errorExtensions = err.extensions as ErrorExtensions;
+            const serverMessage = errorExtensions.response?.message;
+
+            const finalMessage = Array.isArray(serverMessage)
+              ? serverMessage.join(', ')
+              : serverMessage || err.message;
+
+            return <p key={i}>{finalMessage}</p>;
+          })
+        ) : null}
+      </div>
       <div>
         <button type="submit" disabled={loading}>
           {loading ? t('common:loading') : t('authorisation:login')}
