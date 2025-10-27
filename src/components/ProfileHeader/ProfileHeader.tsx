717e313484ca3a7ec2d19c769c46480d5e07d341
Polina <polina.solodkova@gmail.com>
2025-10-27 22:01:09 +0300

    feat:  create profile page

---
+++ src/components/ProfileHeader/ProfileHeader.tsx
@@ -0,0 +1,91 @@
+import { useTranslation } from 'react-i18next';
+import { Button, ButtonGroup } from '@mui/material';
+// import { AppRoutes } from '../../router/router';
+import theme from '../../theme/theme';
+import type React from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import { AppRoutes } from '../../router/router';
+
+interface ProfileHeaderProps {
+  active: 'profile' | 'skills' | 'languages';
+}
+
+const ProfileHeader: React.FC<ProfileHeaderProps> = ({ active }) => {
+  const { userId } = useParams<{ userId: string }>();
+  const navigate = useNavigate();
+
+  const { t } = useTranslation('common');
+  return (
+    <div>
+      <ButtonGroup
+        variant="text"
+        sx={{
+          width: '100%',
+          justifyContent: 'center',
+          gap: 2,
+          '& .MuiButton-root': {
+            width: 180,
+            fontSize: 14,
+            paddingY: theme.spacing(4),
+          },
+        }}
+      >
+        <Button
+          variant="outlined"
+          onClick={() => navigate(AppRoutes.PROFILE.create(userId || ''))}
+          sx={{
+            color:
+              active === 'profile'
+                ? theme.palette.text.secondary
+                : theme.palette.text.disabled,
+            borderBottom:
+              active === 'profile'
+                ? `2px solid ${theme.palette.text.secondary}`
+                : 'none',
+
+            borderRadius: 0,
+          }}
+        >
+          {t('profile')}
+        </Button>
+
+        <Button
+          variant="outlined"
+          //   onClick={() => navigate(AppRoutes.REGISTRATION)}
+          sx={{
+            color:
+              active === 'skills'
+                ? theme.palette.text.secondary
+                : theme.palette.text.disabled,
+            borderBottom:
+              active === 'skills'
+                ? `2px solid ${theme.palette.text.secondary}`
+                : 'none',
+            borderRadius: 0,
+          }}
+        >
+          {t('skills')}
+        </Button>
+        <Button
+          variant="outlined"
+          //   onClick={() => navigate(AppRoutes.REGISTRATION)}
+          sx={{
+            color:
+              active === 'languages'
+                ? theme.palette.text.secondary
+                : theme.palette.text.disabled,
+            borderBottom:
+              active === 'languages'
+                ? `2px solid ${theme.palette.text.secondary}`
+                : 'none',
+            borderRadius: 0,
+          }}
+        >
+          {t('languages')}
+        </Button>
+      </ButtonGroup>
+    </div>
+  );
+};
+
+export default ProfileHeader;
