1237bc4c96617232df2a5e6ee938c9c6184bca36
Polina <polina.solodkova@gmail.com>
2025-11-03 19:36:12 +0300

    feat: implement update cv functionality

---
+++ src/graphql/mutations/updateCV.ts
@@ -0,0 +1,26 @@
+import { gql } from '@apollo/client';
+import { useMutation } from '@apollo/client/react';
+import type { Cv, UpdateCvInput } from 'cv-graphql';
+
+export const UPDATE_CV = gql`
+  mutation UpdateCv($cv: UpdateCvInput!) {
+    updateCv(cv: $cv) {
+      id
+      name
+      education
+      description
+    }
+  }
+`;
+
+export type AppCvArgs = {
+  cv: UpdateCvInput;
+};
+
+export type AddCvResult = {
+  newCv: Cv;
+};
+
+export const useLazyUpdateCv = () => {
+  return useMutation<AddCvResult, AppCvArgs>(UPDATE_CV);
+};
