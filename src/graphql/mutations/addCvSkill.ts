b06e7437745a8755501c1674744478b912f55b90
Polina <polina.solodkova@gmail.com>
2025-11-05 17:49:27 +0300

    feat: implement add cv skill functionality

---
+++ src/graphql/mutations/addCvSkill.ts
@@ -0,0 +1,32 @@
+import { gql } from '@apollo/client';
+import { useMutation } from '@apollo/client/react';
+import type { AddCvSkillInput, Cv } from 'cv-graphql';
+
+export const ADD_CV_SKILL = gql`
+  mutation AddCvSkill($skill: AddCvSkillInput!) {
+    addCvSkill(skill: $skill) {
+      id
+      name
+      user {
+        id
+      }
+      skills {
+        name
+        categoryId
+        mastery
+      }
+    }
+  }
+`;
+
+export type createSkillArgs = {
+  skill: AddCvSkillInput;
+};
+
+export type CreateSkillResult = {
+  Cv: Cv;
+};
+
+export const useLazyAddCvSkill = () => {
+  return useMutation<CreateSkillResult, createSkillArgs>(ADD_CV_SKILL);
+};
